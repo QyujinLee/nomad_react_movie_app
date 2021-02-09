1df4056cf0c1fdea866a63876bce4f2fd0a00217
QyujinLee <gyujin89@gmail.com>
2021-02-09 17:47:51 +0900

    #3 STATE

--- src/App.js
+++ src/App.js
@@ -1,60 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
-    return (
-        <>
-            <h1>I love {name}</h1>
-            <h4>{rating}/5.0</h4>
-            <h2>{picture}</h2>
-        </>
-    );
-}
+class App extends React.Component {
+    state = {
+        isLoading: true,
+    };
 
-const foodILike = [
-    {
-        id: 1,
-        name: "kimchi",
-        image: "kimchi",
-    },
-    {
-        id: 2,
-        name: "samgyupsal",
-        image: "samgyupsal",
-        rating: 4.9,
-    },
-    {
-        id: 3,
-        name: "chukumi",
-        image: "chukumi",
-        rating: 4.8,
-    },
-];
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ isLoading: false });
+        }, 4000);
+    }
 
-function renderFood(dish) {
-    return (
-        <Food
-            key={dish.id}
-            name={dish.name}
-            picture={dish.image}
-            rating={dish.rating}
-        />
-    );
-}
-
-Food.propTypes = {
-    name: PropTypes.string.isRequired,
-    picture: PropTypes.string.isRequired,
-    rating: PropTypes.number,
-};
-
-function App() {
-    return (
-        <div>
-            <h1>Hello</h1>
-            <div>{foodILike.map(renderFood)}</div>
-        </div>
-    );
+    render() {
+        const { isLoading } = this.state;
+        return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+    }
 }
 
 export default App;
