08e6f8dc007fe16aa0b8eddba7be85a0eb16c074
gyujinLee <gyujin89@gmail.com>
2021-02-12 17:58:34 +0900

    #4 Making the movie app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
 import axios from "axios";
+import React from "react";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
     state = {
@@ -26,28 +26,33 @@ class App extends React.Component {
         this.getMovies();
     }
 
-    renderMovies = () => {
-        const { movies } = this.state;
-
-        console.log(movies);
-
-        return movies.map((movie) => {
-            return (
-                <Movie
-                    key={movie.id}
-                    id={movie.id}
-                    year={movie.year}
-                    title={movie.title}
-                    summary={movie.summary}
-                    poster={movie.medium_cover_image}
-                />
-            );
-        });
-    };
-
     render() {
-        const { isLoading } = this.state;
-        return <div>{isLoading ? "Loading" : this.renderMovies}</div>;
+        const { isLoading, movies } = this.state;
+        return (
+            <section className="container">
+            {isLoading ? (
+                <div className="loader">
+                    <span className="loader__text">Loading...</span>
+                </div>
+                ) : (
+                <div className="movies">
+                    {movies.map((movie) => (
+                        <Movie
+                            key={movie.id}
+                            id={movie.id}
+                            year={movie.year}
+                            title={movie.title}
+                            summary={movie.summary}
+                            poster={movie.medium_cover_image} 
+                            genres={movie.genres}
+                        />
+                        ))
+                    }
+                </div>
+                )
+            }
+        </section>
+        );
     }
 }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,26 @@
 import react from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster }) {
-    return <h5>{title}</h5>;
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movies__movie">
+            <img src={poster} alt={title} title={title}></img>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <p className="movie__summary">{summary.slice(0, 180)}...</p>
+                <ul className="movie__genres">
+                    {genres.map((genre, index) => (
+                        <li className="genres__genre" key={index}>
+                            {genre}
+                        </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 Movie.propTypes = {
@@ -11,6 +29,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
