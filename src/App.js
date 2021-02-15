896c75ffc211a3e8f4b5378c200d29313468b20d
QyujinLee <gyujin89@gmail.com>
2021-02-15 16:14:10 +0900

    - 실전형 리액트 Hooks 10개 실습예제
    #1.0 Introduction to useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,77 @@
-import React from "react";
-import { HashRouter, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Detail from "./routes/Detail";
-import Navigation from "./components/Navigation";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+    const [item, setItem] = useState(1);
+    const incrementItem = () => setItem(item + 1);
+    const decrementItem = () => setItem(item - 1);
+
     return (
-        <HashRouter>
-            <Navigation />
-            <Route path="/" exact={true} component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/movie/:id" component={Detail} />
-        </HashRouter>
+        <div className="App">
+            <h1>Hello {item}</h1>
+            <h2>Start editing to see some magic happen!</h2>
+            <button onClick={incrementItem}>Increment</button>
+            <button onClick={decrementItem}>Decrement</button>
+        </div>
     );
-}
+};
 
 export default App;
+
+// class AppUgly extends React.Component {
+//   state = {
+//     item : 1
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello {this.state.item}</h1>
+//         <h2>Start editing to see some magic happen!</h2>
+//         <button onClick={this.incrementItem}>Increment</button>
+//         <button onClick={this.decrementItem}>Decrement</button>
+//       </div>
+//     );
+//   }
+
+//   incrementItem = () => {
+//     this.setState(state => {
+//       return{
+//         item: state.item + 1
+//       }
+//     })
+//   }
+
+//   decrementItem = () => {
+//     this.setState(state => {
+//       return{
+//         item: state.item - 1
+//       }
+//     })
+//   }
+
+// }
+
+// export default AppUgly;
+
+// /**
+//  * nomad_coder_react_movie_app ***
+//  */
+// import React from "react";
+// import { HashRouter, Route } from "react-router-dom";
+// import Home from "./routes/Home";
+// import About from "./routes/About";
+// import Detail from "./routes/Detail";
+// import Navigation from "./components/Navigation";
+
+// function App() {
+//     return (
+//         <HashRouter>
+//             <Navigation />
+//             <Route path="/" exact={true} component={Home} />
+//             <Route path="/about" component={About} />
+//             <Route path="/movie/:id" component={Detail} />
+//         </HashRouter>
+//     );
+// }
+
+// export default App;
