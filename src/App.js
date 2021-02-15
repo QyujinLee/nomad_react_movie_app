55b421cdd16b8b2fe3f8f76a2d87badfd4a9806c
QyujinLee <gyujin89@gmail.com>
2021-02-15 16:49:25 +0900

    - 실전형 리액트 Hooks 10개 실습예제
    #1.1 useInput
    #1.2 useInput part Two

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
+import { useInput } from "./useInput";
 
 const App = () => {
-    const [item, setItem] = useState(1);
-    const incrementItem = () => setItem(item + 1);
-    const decrementItem = () => setItem(item - 1);
+    // const maxLen = (value) => value.length <= 10;
+    const maxLen = (value) => !value.includes("@");
+    const name = useInput("Mr.", maxLen);
 
     return (
         <div className="App">
-            <h1>Hello {item}</h1>
-            <h2>Start editing to see some magic happen!</h2>
-            <button onClick={incrementItem}>Increment</button>
-            <button onClick={decrementItem}>Decrement</button>
+            <h1>Hello</h1>
+            <input placeholder="Name" {...name} />
         </div>
     );
 };
