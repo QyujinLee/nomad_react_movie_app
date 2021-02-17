e2ec0e4df6a8b0941b733a3dd6466f80c5855cac
QyujinLee <gyujin89@gmail.com>
2021-02-17 13:28:03 +0900

    - 실전형 리액트 Hooks 10개 실습예제
    #2.7 useNotification

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useScroll } from "./useScroll";
-import { useFullscreen } from "./useFullscreen";
+import { useNotification } from "./useNotification";
 
 const App = () => {
-    const { y } = useScroll();
-    const onFullS = (isFull) => {
-        console.log(isFull ? "We are full" : "We are small");
+    const customOptions = {
+        body: "I love it",
     };
-    const { element, triggerFull, exitFull } = useFullscreen(onFullS);
-
+    const triggerNofif = useNotification("Can I Steel It ?", customOptions);
     return (
         <div className="App" style={{ height: "1000vh" }}>
-            <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
-                hello
-            </h1>
-            <div ref={element}>
-                <img
-                    src="http://akns-images.eoline.com/eol_image"
-                    alt="testImg"
-                />
-            </div>
-            <button onClick={triggerFull}>Make fullscreen</button>
-            <button onClick={exitFull}>Exit fullscreen</button>
+            <button onClick={triggerNofif}>hello</button>
         </div>
     );
 };
