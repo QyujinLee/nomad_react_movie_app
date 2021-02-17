6a5c90a16429cc26a7574c5fc88846182cd223b7
QyujinLee <gyujin89@gmail.com>
2021-02-17 17:03:43 +0900

    return to movie app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,22 @@
-import React, { useState, useEffect, useRef } from "react";
-import useAxios from "./useAxios/useAxios";
+/**
+ * nomad_coder_react_movie_app ***
+ */
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
-const App = () => {
-    const { loading, data, refetch } = useAxios({
-        url: "https://yts.am/api/v2/list_movies.json",
-    });
-
+function App() {
     return (
-        <div className="App" style={{ height: "1000vh" }}>
-            <h1>{data && data.status}</h1>
-            <h2>{loading && "Loading"}</h2>
-            <button onClick={refetch}>Refetch</button>
-        </div>
+        <HashRouter>
+            <Navigation />
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/movie/:id" component={Detail} />
+        </HashRouter>
     );
-};
+}
 
 export default App;
-
-// /**
-//  * nomad_coder_react_movie_app ***
-//  */
-// import React from "react";
-// import { HashRouter, Route } from "react-router-dom";
-// import Home from "./routes/Home";
-// import About from "./routes/About";
-// import Detail from "./routes/Detail";
-// import Navigation from "./components/Navigation";
-
-// function App() {
-//     return (
-//         <HashRouter>
-//             <Navigation />
-//             <Route path="/" exact={true} component={Home} />
-//             <Route path="/about" component={About} />
-//             <Route path="/movie/:id" component={Detail} />
-//         </HashRouter>
-//     );
-// }
-
-// export default App;
