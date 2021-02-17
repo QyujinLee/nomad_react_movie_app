fb35179e63bc701e71c232d4f5085911d27ba386
QyujinLee <gyujin89@gmail.com>
2021-02-17 11:27:40 +0900

    - 실전형 리액트 Hooks 10개 실습예제
    #2.6 useScroll & useFullscreen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useFadeIn } from "./useFadeIn";
-import { useNetwork } from "./useNetwork";
+import { useScroll } from "./useScroll";
+import { useFullscreen } from "./useFullscreen";
 
 const App = () => {
-    const fadeInH1 = useFadeIn(1, 2);
-    const fadeInP = useFadeIn(5, 10);
-    const handleNetworkChange = (online) => {
-        console.log(online ? "We just went online" : "We are offline");
+    const { y } = useScroll();
+    const onFullS = (isFull) => {
+        console.log(isFull ? "We are full" : "We are small");
     };
-    const onLine = useNetwork(handleNetworkChange);
+    const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+
     return (
-        <div className="App">
-            <h1 {...fadeInH1}>FadeIn 1</h1>
-            <p {...fadeInP}>FadeIn 2</p>
-            <br />
-            <h1>{onLine ? "Online" : "Offline"}</h1>
+        <div className="App" style={{ height: "1000vh" }}>
+            <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
+                hello
+            </h1>
+            <div ref={element}>
+                <img
+                    src="http://akns-images.eoline.com/eol_image"
+                    alt="testImg"
+                />
+            </div>
+            <button onClick={triggerFull}>Make fullscreen</button>
+            <button onClick={exitFull}>Exit fullscreen</button>
         </div>
     );
 };
